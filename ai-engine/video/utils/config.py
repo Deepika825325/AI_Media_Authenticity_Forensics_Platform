ccc527178b4c354f5e7199739b9ae95a3d096d90
Deepika825325 <deepikakri3103@gmail.com>
2026-05-23 12:58:34 +0530

    Updated colab configuration

--- ai-engine/video/utils/config.py
+++ ai-engine/video/utils/config.py
@@ -1,33 +1,32 @@
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).resolve().parents[3]
+ROOT_DIR = Path("/content/AI_Media_Authenticity_Forensics_Platform")
 
-# =========================================
-# DATA
-# =========================================
+DATASET_DIR = Path(
+    "/content/drive/MyDrive/DeepVerify/datasets"
+)
+
+CHECKPOINT_DIR = Path(
+    "/content/drive/MyDrive/DeepVerify/checkpoints"
+)
+
+OUTPUT_DIR = Path(
+    "/content/drive/MyDrive/DeepVerify/outputs"
+)
+
+LOG_DIR = Path(
+    "/content/drive/MyDrive/DeepVerify/logs"
+)
 
 CSV_PATH = (
-    ROOT_DIR /
-    "datasets/metadata/final_dataset.csv"
+    DATASET_DIR /
+    "metadata/final_dataset.csv"
 )
 
 IMAGE_SIZE = 224
+
 BATCH_SIZE = 16
 
-# =========================================
-# TRAINING
-# =========================================
-
 EPOCHS = 10
+
 LEARNING_RATE = 1e-4
-
-# =========================================
-# PATHS
-# =========================================
-
-CHECKPOINT_DIR = ROOT_DIR / "checkpoints"
-
-CHECKPOINT_DIR.mkdir(
-    parents=True,
-    exist_ok=True
-)
